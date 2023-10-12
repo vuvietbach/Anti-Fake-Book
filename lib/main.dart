599fcc8500138542276d504c55a2b441aeb44292
vuvietbach <vuvietbach12345@gmail.com>
2023-10-12 16:45:54 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:anti_fake_book/page.dart';
+import 'package:anti_fake_book/test.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Name()));
+}
