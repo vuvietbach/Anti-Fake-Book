e9ceec736ab98b85c346ec3ed20ab7d673159b6f
vuvietbach <vuvietbach12345@gmail.com>
2023-10-22 16:17:29 +0700

    refract code sign in sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:anti_fake_book/HomePage.dart';
-import 'package:anti_fake_book/test.dart';
-import "SignIn.dart";
-import 'SignUp/SignUp.dart';
+import 'package:anti_fake_book/screen/SignIn.dart';1
+import 'package:anti_fake_book/screen/SignUp/SignUp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(MaterialApp(localizationsDelegates: const [
+  runApp(const MaterialApp(localizationsDelegates: [
     GlobalMaterialLocalizations.delegate,
     GlobalWidgetsLocalizations.delegate,
     GlobalCupertinoLocalizations.delegate,
-
-  ], supportedLocales: const [
+  ], supportedLocales: [
     Locale('en', ''),
     Locale('vi', ''), // Vietnamese locale
-  ], debugShowCheckedModeBanner: false, home: const SignUp()));
+  ], debugShowCheckedModeBanner: false, home: SignUp()));
 }
