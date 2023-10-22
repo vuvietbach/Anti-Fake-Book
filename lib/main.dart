e48a10602e83f621b5d8d9e068849bff5c28e61d
Nguyen Minh Tuan <nguyen.minh.tuan@azoom.jp>
2023-10-22 22:55:37 +0700

    :+1: Add router

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
-import 'SignUp/SignUp.dart';
+import 'package:anti_fake_book/layout/Empty.dart';
+import 'package:anti_fake_book/screen/HomeFake.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:anti_fake_book/screen/CreatePost.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(MaterialApp(localizationsDelegates: const [
-    GlobalMaterialLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
+final GoRouter _router = GoRouter(routes: [
+  GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState stage) {
+        return const EmptyLayout(child: HomeFake());
+      },
+      routes: [
+        GoRoute(
+            path: 'post/create',
+            builder: (BuildContext context, GoRouterState stage) =>
+                CreatePostScreen())
+      ]),
+]);
 
-  ], supportedLocales: const [
-    Locale('en', ''),
-    Locale('vi', ''), // Vietnamese locale
-  ], debugShowCheckedModeBanner: false, home: const SignUp()));
-}
+void main() => runApp(MaterialApp.router(routerConfig: _router));
