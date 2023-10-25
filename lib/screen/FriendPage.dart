c0d633b23035e2dab24516da7cbc8709c1b2bd39
OnlySekai <91949320+OnlySekai@users.noreply.github.com>
2023-10-25 23:58:56 +0700

    refactor + invite tab (#8)
    Co-authored-by: quang13542 <quang300102@gmail.com>

--- lib/screen/FriendPage.dart
+++ lib/screen/FriendPage.dart
@@ -16,7 +16,7 @@ class _FriendPageContentState extends State<FriendPageContent> {
 
   final List<Friend> friendRequests = [
     Friend('Friend 1', 'assets/images/PostImage_01.jpeg'),
-    Friend('Friend 2', 'assets/images/PostImage_02.jpeg'),
+    Friend('Nguyen Van A', 'assets/images/PostImage_02.jpeg'),
     // Danh sách lời mời kết bạn
   ];
 
@@ -28,92 +28,119 @@ class _FriendPageContentState extends State<FriendPageContent> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        if (index < friendRequests.length) {
-          return Container(
-              color: Colors.white,
-              margin: EdgeInsets.only(bottom: 10),
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // User avatar and name
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 48,
-                        backgroundImage: AssetImage(friendRequests[index].imageUrl),
-                      ),
-                      SizedBox(width: 14),
-                      Column(
+    int totalFriendRequests = friendRequests.length;
+
+    return Container(
+      color: Colors.white,
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Lời mời kết bạn: $totalFriendRequests',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 100,
+            child:ListView.builder(
+              itemCount: friendRequests.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index < friendRequests.length) {
+                  return Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 10),
+                      padding: EdgeInsets.all(10),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          SizedBox(height: 16),
-                          Row(
-                              children: [
-                                Text(
-                                  friendRequests[index].name,
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold
-                                  ),
-                                ),
-                                Text(
-                                  '2 ngày',
-                                  style: TextStyle(
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.grey[400],
-                                  ),
-                                ),
-                              ]
-                          ),
-                          SizedBox(height: 5),
+                          // User avatar and name
                           Row(
                             children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle button press here
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: Size(130, 38), // Đặt độ dài và chiều cao của nút
-                                ),
-                                child: Text(
-                                  'Chấp nhận',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
+                              CircleAvatar(
+                                radius: 48,
+                                backgroundImage: AssetImage(friendRequests[index].imageUrl),
                               ),
-                              SizedBox(width: 10),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle button press here
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: Size(130, 38),
-                                  backgroundColor: Colors.grey[300],
-                                ),
-                                child: Text(
-                                  'Xóa',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.black, // Đổi màu chữ sang màu đen
+                              SizedBox(width: 14),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 16),
+                                  Container(
+                                    width: 270,
+                                    child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            friendRequests[index].name,
+                                            style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold
+                                            ),
+                                          ),
+                                          Text(
+                                            '2 ngày',
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey[400],
+                                            ),
+                                          ),
+                                        ]
+                                    ),
                                   ),
-                                ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          // Handle button press here
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          minimumSize: Size(130, 38), // Đặt độ dài và chiều cao của nút
+                                        ),
+                                        child: Text(
+                                          'Chấp nhận',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          // Handle button press here
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          minimumSize: Size(130, 38),
+                                          backgroundColor: Colors.grey[300],
+                                        ),
+                                        child: Text(
+                                          'Xóa',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.black, // Đổi màu chữ sang màu đen
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ],
                           ),
                         ],
-                      ),
-                    ],
-                  ),
-                ],
-              )
-          );
-        }
-      },
+                      )
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      )
     );
   }
 }
