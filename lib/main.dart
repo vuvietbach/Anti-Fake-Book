11a3fe275a055553826e38cbf6894b92628ac792
vuvietbach <vuvietbach12345@gmail.com>
2023-10-25 11:52:53 +0700

    add router

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
-import 'package:anti_fake_book/HomePage.dart';
-import 'package:anti_fake_book/screen/signIn/SignIn.dart';
-import 'package:anti_fake_book/screen/SignUp/SignUp.dart';
+import 'package:anti_fake_book/screen/CreatePost.dart';
+import 'package:anti_fake_book/screen/HomeFake.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:go_router/go_router.dart';
+import 'package:anti_fake_book/layout/Empty.dart';
 
-import 'screen/welcomeScreen.dart';
+final GoRouter _router = GoRouter(routes: [
+  GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState stage) {
+        return const EmptyLayout(child: HomeFake());
+      },
+      routes: [
+        GoRoute(
+            path: 'post/create',
+            builder: (BuildContext context, GoRouterState stage) =>
+                CreatePostScreen())
+      ]),
+]);
 
 void main() {
-  runApp(const MaterialApp(localizationsDelegates: [
-    GlobalMaterialLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-  ], supportedLocales: [
-    Locale('en', ''),
-    Locale('vi', ''), // Vietnamese locale
-  ], debugShowCheckedModeBanner: false, home: SignUp()));
+  runApp(MaterialApp.router(
+    localizationsDelegates: const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ], 
+    supportedLocales: const [
+      Locale('en', ''),
+      Locale('vi', ''),], 
+    debugShowCheckedModeBanner: false,
+    routerConfig: _router
+  ));
 }
