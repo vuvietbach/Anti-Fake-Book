c78926fbb6002b21b48569e7f52be823c9d958e1
quang13542 <quang300102@gmail.com>
2023-10-25 15:44:44 +0700

    invite tab

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -14,58 +14,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 6, // The number of tabs
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("AntiFakebook", style: TextStyle(color: Colors.blue.shade900)),
-          backgroundColor: Colors.white,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search, color: Colors.black),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.message, color: Colors.black),
-            ),
-          ],
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home, color: Colors.black),
+        length: 6, // The number of tabs
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("AntiFakebook", style: TextStyle(color: Colors.blue.shade900)),
+            backgroundColor: Colors.white,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search, color: Colors.black),
               ),
-              Tab(
-                icon: Icon(Icons.people, color: Colors.black),
-              ),
-              Tab(
-                icon: Icon(Icons.video_library, color: Colors.black),
-              ),
-              Tab(
-                icon: Icon(Icons.shop, color: Colors.black),
-              ),
-              Tab(
-                icon: Icon(Icons.notifications, color: Colors.black),
-              ),
-              Tab(
-                icon: Icon(Icons.menu, color: Colors.black),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.message, color: Colors.black),
               ),
             ],
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.people, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.video_library, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.shop, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.notifications, color: Colors.black),
+                ),
+                Tab(
+                  icon: Icon(Icons.menu, color: Colors.black),
+                ),
+              ],
+            ),
           ),
-        ),
-        body: Container(
-          color: Colors.grey[300],
-          child: TabBarView(
-            children: [
-              PostHomePageContent(),
-              VideoPageContent(),
-              FriendPageContent(),
-              ShopPageContent(),
-              NotificationContent(),
-              MenuContent(),
-            ],
+          body: Container(
+            color: Colors.grey[300],
+            child: TabBarView(
+              children: [
+                PostHomePageContent(),
+                FriendPageContent(),
+                VideoPageContent(),
+                ShopPageContent(),
+                NotificationContent(),
+                MenuContent(),
+              ],
+            ),
           ),
-        ),
-      )
+        )
     );
   }
 }
