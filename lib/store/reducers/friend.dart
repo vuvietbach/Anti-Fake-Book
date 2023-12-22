bd216610f574a40e57d03089ed6c5f15f906d0d8
vuvietbach <vuvietbach12345@gmail.com>
2023-12-22 12:31:20 +0700

    format code

--- lib/store/reducers/friend.dart
+++ lib/store/reducers/friend.dart
@@ -1,9 +1,6 @@
-import 'dart:ffi';
-
 import 'package:anti_fake_book/helper/helper.dart';
 import 'package:anti_fake_book/models/base_apis/dto/response/friend.dto.dart';
 import 'package:anti_fake_book/store/actions/friends.dart';
-import 'package:anti_fake_book/store/state/friend/friend_list.dart';
 import 'package:anti_fake_book/store/state/index.dart';
 import 'package:anti_fake_book/utils.dart';
 
